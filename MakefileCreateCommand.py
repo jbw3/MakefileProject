8eadbed0b221186ca61b72347705dbb3abcfcbcb
John Wilkes <jwilkes0011@gmail.com>
2016-12-10 14:41:48 -0600

    Creating makefile from template

--- MakefileCreateCommand.py
+++ MakefileCreateCommand.py
@@ -1,10 +1,46 @@
 import os, textwrap
-import sublime_plugin
+import sublime, sublime_plugin
 
 def reformat(template):
     return textwrap.dedent(template).lstrip()
 
 class MakefileCreateCommand(sublime_plugin.WindowCommand):
+    ''' Command to create a new makefile. '''
+    C_DEFAULT_MAKEFILE = reformat('''
+    INCLUDES =
+    OBJDIR = obj
+
+    CC = gcc
+    CFLAGS = $(INCLUDES)
+
+    LDLIBS = -L<lib path> -l<lib name>
+    LDFLAGS = $(LDLIBS)
+
+    DEPS =
+
+    _OBJ = main.c.o
+    OBJ = $(patsubst %,$(OBJDIR)/%,$(_OBJ))
+
+    TARGET = test
+
+    .PHONY: all
+    all: init $(TARGET)
+
+    .PHONY: init
+    init:
+    \tmkdir -p $(OBJDIR)
+
+    $(TARGET): $(OBJ)
+    \t$(CC) $(OBJ) -o $(TARGET) $(LDFLAGS)
+
+    $(OBJDIR)/%.c.o: %.c $(DEPS)
+    \t$(CC) $(CCFLAGS) -c $< -o $@
+
+    .PHONY: clean
+    clean:
+    \trm -f $(OBJDIR)/*.o $(TARGET)
+    ''')
+
     CPP_DEFAULT_MAKEFILE = reformat('''
     INCLUDES =
     OBJDIR = obj
@@ -17,7 +53,7 @@ class MakefileCreateCommand(sublime_plugin.WindowCommand):
 
     DEPS =
 
-    _OBJ = main.o
+    _OBJ = main.cpp.o
     OBJ = $(patsubst %,$(OBJDIR)/%,$(_OBJ))
 
     TARGET = test
@@ -40,18 +76,66 @@ class MakefileCreateCommand(sublime_plugin.WindowCommand):
     \trm -f $(OBJDIR)/*.o $(TARGET)
     ''')
 
+    DEFAULT_MAKEFILES = {
+        'makefile_c'   : C_DEFAULT_MAKEFILE,
+        'makefile_c++' : CPP_DEFAULT_MAKEFILE,
+    }
+
+    PLUGIN_PATH = os.path.join(sublime.packages_path(), 'User', 'MakefileProject')
+
     def run(self):
+        ''' Run the command. '''
+        self.getMakefileType()
+
+    def getMakefileType(self):
+        ''' Ask the user which type of makefile to create. '''
+        settings = sublime.load_settings('MakefileProject.sublime-settings')
+        types = list(settings.get('templates').keys())
+
+        def onDone(typeIdx):
+            if typeIdx >= 0:
+                t = types[typeIdx]
+                templateFilename = settings.get('templates')[t]['filename']
+                self.getFilename(templateFilename)
+
+        self.window.show_quick_panel(types, onDone)
+
+    def getFilename(self, templateFilename):
+        ''' Ask the user for the filename. '''
         if len(self.window.folders()) > 0:
             initText = os.path.join(self.window.folders()[0], 'makefile')
         else:
             initText = ''
-        self.window.show_input_panel('Filename:', initText, self.createMakefile, None, None)
 
-    def createMakefile(self, filename):
+        def onDone(filename):
+            self.createMakefile(templateFilename, filename)
+
+        self.window.show_input_panel('Filename:', initText, onDone, None, None)
+
+    def createMakefile(self, templateFilename, filename):
+        ''' Create the makefile. '''
         # TODO: validate filename
         # TODO: check if file already exists
         if filename == '':
             return
 
-        with open(filename, 'w') as f:
-            f.write(MakefileCreateCommand.CPP_DEFAULT_MAKEFILE)
+        # ensure default files exist
+        # TODO: Is there a better place to call this?
+        self.createDefaultFiles()
+
+        templatePath = os.path.join(self.PLUGIN_PATH, templateFilename)
+
+        # TODO: Should probably just copy the file
+        with open(templatePath, 'r') as fin, open(filename, 'w') as fout:
+            fout.write(fin.read())
+
+    def createDefaultFiles(self):
+        os.makedirs(self.PLUGIN_PATH, exist_ok=True)
+
+        # loop through all default files and create any that
+        # don't already exist
+        for filename, contents in MakefileCreateCommand.DEFAULT_MAKEFILES.items():
+            filePath = os.path.join(self.PLUGIN_PATH, filename)
+            if not os.path.exists(filePath):
+                with open(filePath, 'w') as f:
+                    f.write(contents)
