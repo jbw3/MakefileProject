448636cfb7686b1fef762d1b893cc188202c3213
John Wilkes <jwilkes0011@gmail.com>
2016-12-09 22:41:26 -0600

    Initial commit

---
+++ Commands.py
@@ -0,0 +1,10 @@
+import sublime
+import sublime_plugin
+
+class MakefileCreateCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        print('TODO: Create makefile')
+
+class MakefileAddFileCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        print('TODO: Add file to makefile')
