de52d9b8cdd7e0c6f43092e423ab587c73a16cae
John Wilkes <jwilkes0011@gmail.com>
2016-12-10 23:49:05 -0600

    Improving file add

--- MakefileParser.py
+++ MakefileParser.py
@@ -1,6 +1,8 @@
 import os, re, tempfile
 
 class MakefileParser(object):
+    SPLIT_PATTERN = re.compile(r'[\s\\]+')
+
     def __init__(self, makefile):
         self.makefile = makefile
 
@@ -9,6 +11,8 @@ class MakefileParser(object):
             for filename in filenames:
                 if filename not in makefileFilenames:
                     makefileFilenames.append(filename)
+            # TODO: Use natural sort
+            makefileFilenames.sort()
 
         self._process(processFiles)
 
@@ -17,17 +21,22 @@ class MakefileParser(object):
         pass
 
     def _process(self, processFiles):
-        pattern = re.compile(r'^_OBJ\s*=')
+        pattern = re.compile(r'^_OBJ[ \t]*=[ \t]*')
 
         with open(self.makefile, 'r') as inFile, tempfile.NamedTemporaryFile(dir=os.path.dirname(self.makefile), delete=False) as outFile:
             line = inFile.readline()
             while line != '':
                 match = re.search(pattern, line)
                 if match is not None:
-                    var = line[:match.end()]
-                    filenames = line[match.end():].split()
-                    processFiles(filenames)
-                    out = '{} {}\n'.format(var, ' '.join(filenames))
+                    fileVarStr = line
+                    readNext = line.rstrip().endswith('\\')
+                    while line != '' and readNext:
+                        line = inFile.readline()
+                        fileVarStr += line
+                        readNext = line.rstrip().endswith('\\')
+
+                    out = self._splitFileString(fileVarStr, match, processFiles)
+
                 else:
                     out = line
 
@@ -38,3 +47,19 @@ class MakefileParser(object):
 
         # atomically replace original file
         os.replace(outFile.name, self.makefile)
+
+    def _splitFileString(self, fileVarStr, varMatch, processFiles):
+        varStr = fileVarStr[:varMatch.end()]
+        fileStr = fileVarStr[varMatch.end():]
+
+        filenames = re.split(self.SPLIT_PATTERN, fileStr)
+        filenames = list(filter(lambda x: x != '', filenames))
+        processFiles(filenames)
+
+        if varStr.endswith('='):
+            varStr += ' '
+        sep = ' \\\n' + ' ' * len(varStr)
+
+        out = '{}{}\n'.format(varStr, sep.join(filenames))
+
+        return out
