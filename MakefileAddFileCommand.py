0079c5752b9948cd67e248df58d5ce808ca94ed2
John Wilkes <jwilkes0011@gmail.com>
2016-12-10 21:09:56 -0600

    Can now specify files to add to makefile

--- MakefileAddFileCommand.py
+++ MakefileAddFileCommand.py
@@ -1,13 +1,20 @@
 import os, re, tempfile
-import sublime_plugin
+import sublime, sublime_plugin
 
 class MakefileAddFileCommand(sublime_plugin.WindowCommand):
-    def run(self):
+    def run(self, paths=[]):
         makefile = self.getMakefile()
         if makefile == '':
+            sublime.error_message('Could not find makefile')
             return
 
-        self.addFile(makefile, 'test.cpp')
+        if len(paths) == 0:
+            def onDone(filename):
+                # TODO: validate filename
+                self.addFiles(makefile, [filename])
+            self.window.show_input_panel('File:', os.path.dirname(makefile) + os.sep, onDone, None, None)
+        else:
+            self.addFiles(makefile, paths)
 
     def getMakefile(self):
         projData = self.window.project_data()
@@ -18,13 +25,17 @@ class MakefileAddFileCommand(sublime_plugin.WindowCommand):
                 return filename
         return ''
 
-    def addFile(self, makefile, filename):
+    def addFiles(self, makefile, paths):
+        filenames = []
+        for path in paths:
+            filenames.append(os.path.basename(path) + '.o')
+
         with open(makefile, 'r') as fin, tempfile.NamedTemporaryFile(dir=os.path.dirname(makefile), delete=False) as fout:
             line = fin.readline()
             while line != '':
                 match = re.search(r'^_OBJ\s*=', line)
                 if match is not None:
-                    out = '{} {}.o\n'.format(line.rstrip(), filename)
+                    out = '{} {}\n'.format(line.rstrip(), ' '.join(filenames))
                 else:
                     out = line
 
